f491e9ac524b9e2589f561d33defda3325aa0499
feelingslw <wei147852>
2018-12-12 23:56:39 +0800

    first

---
+++ config.py
@@ -0,0 +1,4 @@
+DEBUG=True
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123@localhost:3306/book?charset=utf8'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
---
+++ run.py
@@ -0,0 +1,5 @@
+from book import app
+
+
+
+app.run()
