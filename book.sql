fcd933f4be5a4f292977779564358037d5df6f6a
feelingslw <wei147852>
2018-12-13 23:44:59 +0800

    1

--- book.sql
+++ book.sql
@@ -5,13 +5,13 @@
  Source Server Type    : MySQL
  Source Server Version : 80012
  Source Host           : localhost:3306
- Source Schema         : book_sys
+ Source Schema         : book
 
  Target Server Type    : MySQL
  Target Server Version : 80012
  File Encoding         : 65001
 
- Date: 13/12/2018 00:05:27
+ Date: 13/12/2018 23:44:43
 */
 
 SET NAMES utf8mb4;
@@ -45,7 +45,14 @@ CREATE TABLE `t_book` (
   `buy_date` date NOT NULL COMMENT '入库日期',
   `brief` varchar(100) DEFAULT NULL COMMENT '内容摘要',
   PRIMARY KEY (`book_num`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_book
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_book` VALUES (1, 'Python', 'zs', '1', 1, '图灵', '2018-01-01', 2, 2, '2018-01-01', '100');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for t_book_student
@@ -90,7 +97,37 @@ CREATE TABLE `t_sort` (
   `sort_id` int(11) NOT NULL AUTO_INCREMENT COMMENT '类别id',
   `sort_name` varchar(30) NOT NULL COMMENT '类别名',
   PRIMARY KEY (`sort_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of t_sort
+-- ----------------------------
+BEGIN;
+INSERT INTO `t_sort` VALUES (1, '计算机理论硬件');
+INSERT INTO `t_sort` VALUES (2, '外部设备');
+INSERT INTO `t_sort` VALUES (3, '维修操作系统/系统开发');
+INSERT INTO `t_sort` VALUES (4, '数据库');
+INSERT INTO `t_sort` VALUES (5, '程序设计');
+INSERT INTO `t_sort` VALUES (6, '网络与数据通信');
+INSERT INTO `t_sort` VALUES (7, '图形图像 多媒体');
+INSERT INTO `t_sort` VALUES (8, 'CAD CAM CAE');
+INSERT INTO `t_sort` VALUES (9, '软件工程/开发项目管理');
+INSERT INTO `t_sort` VALUES (10, '行业软件及应用');
+INSERT INTO `t_sort` VALUES (11, '人工智能');
+INSERT INTO `t_sort` VALUES (12, '家庭与办公室用书');
+INSERT INTO `t_sort` VALUES (13, '计算机考试');
+INSERT INTO `t_sort` VALUES (14, '认证管理信息系统(MIS)');
+INSERT INTO `t_sort` VALUES (15, '地理信息管理系统（GIS)');
+INSERT INTO `t_sort` VALUES (16, '企业软件开发与实施');
+INSERT INTO `t_sort` VALUES (17, '信息安全');
+INSERT INTO `t_sort` VALUES (18, '项目管理IT人文');
+INSERT INTO `t_sort` VALUES (19, '电脑杂志——合订本');
+INSERT INTO `t_sort` VALUES (20, '计算机体系结构');
+INSERT INTO `t_sort` VALUES (21, '数码全攻略');
+INSERT INTO `t_sort` VALUES (22, '影印版');
+INSERT INTO `t_sort` VALUES (23, '计算机教材');
+INSERT INTO `t_sort` VALUES (24, '英文原版书-计算机');
+COMMIT;
 
 -- ----------------------------
 -- Table structure for t_student
